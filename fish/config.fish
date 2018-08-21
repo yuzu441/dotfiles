f4962fcad37484266c6c4564608e175f576ebb9c
yuzu <s.yuzu441@gmail.com>
2018-08-21 23:14:08 +0900

    Androidの設定

--- fish/config.fish
+++ fish/config.fish
@@ -2,4 +2,9 @@ set -x PATH $HOME/.nodebrew/current/bin $PATH
 set -x PATH $HOME/.cargo/bin $PATH
 set -x PATH $HOME/bin $PATH
 
+set -x ANDROID_HOME $HOME/Android/Sdk
+set -x PATH $ANDROID_HOME/tools $PATH
+set -x PATH $ANDROID_HOME/tools/bin $PATH
+set -x PATH $ANDROID_HOME/platform-tools
+
 rbenv init - | source
