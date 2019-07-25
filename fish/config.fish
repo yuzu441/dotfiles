9e342375dea45601827f9b11b913f7e9915a344e
yuzu <s.yuzu441@gmail.com>
2019-07-25 23:27:47 +0900

    gopath削除

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,5 @@
 set -x ANDROID_HOME $HOME/Android/Sdk
-set -x GOENV_ROOT $HOME/.goenv
-set -x GOPATH $HOME/develop
+set -x GO111MODULE on
 
 set -x PATH $PATH \
   $HOME/.nodebrew/current/bin \
@@ -10,10 +9,9 @@ set -x PATH $PATH \
   $ANDROID_HOME/tools \
   $ANDROID_HOME/tools/bin \
   $ANDROID_HOME/platform-tools \
-  $GOENV_ROOT/bin \
-  $GOPATH/bin
+  /usr/local/go/bin \
+  $HOME/go/bin
 
 alias nodejs=node
 gl='git log --oneline --decorate --graph --branches --tags --remotes'
 
-status --is-interactive; and source (goenv init -|psub)
