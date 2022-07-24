2317d8171901fa506cfa4d3cb8ff4bfdba5352b8
yuzu <s.yuzu441@gmail.com>
2022-07-24 17:34:14 +0900

    Fix doesnt work yarn

--- fish/config.fish
+++ fish/config.fish
@@ -1,14 +1,14 @@
 alias ls=exa
 alias ll='ls -al'
 alias cat=bat
-alias yarn='yarn --use-yarnrc "$XDG_CONFIG_HOME/yarn/config"'
+#alias yarn='yarn --use-yarnrc "$XDG_CONFIG_HOME/yarn/config"'
 alias gl='git log --oneline --decorate --graph --branches --tags --remotes'
 
 switch (uname)
 case Darwin
   eval (/opt/homebrew/bin/brew shellenv)
 case '*'
-  echo none
+  #echo none
 end
 
 if status is-interactive
