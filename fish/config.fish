c12db49a40dd1175703b258db47833b02d02158e
yuzu <s.yuzu441@gmail.com>
2023-06-25 15:16:21 +0900

    fix: reading file order

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,13 @@ alias ls=exa
 alias ll='ls -al'
 alias gl='git log --oneline --decorate --graph --branches --tags --remotes'
 
+switch (uname)
+case Darwin
+  eval (/opt/homebrew/bin/brew shellenv)
+case '*'
+  #echo none
+end
+
 if type bat >> /dev/null
   alias cat=bat
 else if type batcat >> /dev/null
@@ -9,12 +16,7 @@ else if type batcat >> /dev/null
   alias cat=batcat
 end
 
-switch (uname)
-case Darwin
-  eval (/opt/homebrew/bin/brew shellenv)
-case '*'
-  #echo none
-end
+
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
